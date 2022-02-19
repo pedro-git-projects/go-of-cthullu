f938371c8292d61e296c6768a85ec78cfc0553da
pedro-git-projects <pedro.generic.email@gmail.com>
2022-02-19 13:56:55 -0300

    remove new lines

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/pedro-git-projects/go-of-cthullu/pkg/investigators"
 )
@@ -13,6 +14,7 @@ func main() {
 	r := bufio.NewReader(os.Stdout)
 	fmt.Println("Please input the name of your investigator:")
 	name, _ := r.ReadString('\n')
+	name = strings.TrimSuffix(name, "\n")
 
 	fmt.Println("Please input the age of your investigator:")
 	var age int
@@ -20,12 +22,15 @@ func main() {
 
 	fmt.Println("Please input the birthplace of your investigator:")
 	birth, _ := r.ReadString('\n')
+	birth = strings.TrimSuffix(birth, "\n")
 
 	fmt.Println("Please input the residence of your investigator:")
 	residence, _ := r.ReadString('\n')
+	residence = strings.TrimSuffix(residence, "\n")
 
 	fmt.Println("Please input the occupation of your investigator:")
 	occupation, _ := r.ReadString('\n')
+	occupation = strings.TrimSuffix(occupation, "\n")
 
 	investigators.CreateInvestigator(&character, name, age, birth, residence, occupation)
 
